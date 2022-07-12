9bd6c401d95a9f4ecb38f632bfe395aba67fad0c
gLitCH1904 <95301749+gLitCH1904@users.noreply.github.com>
2022-07-12 21:23:36 +0100

    Add files via upload

---
+++ interactive.js
@@ -0,0 +1,33 @@
+"use strict";
+
+const container1 = document.querySelector(".container-1");
+const container2 = document.querySelector(".container-2");
+const rateBtns = document.querySelectorAll(".rating");
+const submitBtn = document.querySelector(".submit");
+const overlay = document.querySelector(".overlay");
+const number = document.querySelector(".number");
+const cont = document.querySelector(".container-2.active");
+console.log(overlay);
+
+// SUBMIT BUTTON FUNCTIONALITY
+function submitRate() {
+  container1.classList.toggle("active");
+  container2.classList.toggle("active");
+}
+submitBtn.addEventListener("click", submitRate);
+
+overlay.addEventListener("click", () => {
+  container1.classList.remove("active");
+  container2.classList.remove("active");
+  for (let i = 0; i < rateBtns.length; i++) {
+    rateBtns[i].classList.remove("active");
+  }
+});
+
+// FUNCTIONALITY OF RATE BUTTONS
+for (let i = 0; i < rateBtns.length; i++) {
+  rateBtns[i].addEventListener("click", () => {
+    number.textContent = rateBtns[i].textContent;
+    rateBtns[i].classList.toggle("active");
+  });
+}
